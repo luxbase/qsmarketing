c3ed06eaf90bcdc45bdc57116df06350c4256a99
danieloceno <daniel.oceno@outlook.com>
2026-09-17 07:49:36 -0400

    feat(core): add NOTIFICATION_EMAIL setting for lead alerts

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const WHATSAPP_NUMBER = '595971153698';
 export const WHATSAPP_MESSAGE = encodeURIComponent('Hola Qualis Studio! Quisiera recibir información sobre sus servicios de staging digital.');
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`;
 export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwo7eK6PQPhsiAo1q0rTu9VGSKngFmfWRmv4ggfnU2a5pB3Czrhsuf5tCtjI-HlHdnulA/exec';
+export const NOTIFICATION_EMAIL = 'romina.oceno@gmail.com';
