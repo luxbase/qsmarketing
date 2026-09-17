e1fd1f2f094940b5a82418102694397961734010
danieloceno <daniel.oceno@outlook.com>
2026-09-17 06:03:06 -0400

    fix(core): fix github pages deployment workflow and astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+const getBase = () => {
+    if (process.env.GITHUB_REPOSITORY) {
+        return `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`;
+    }
+    return '/qsmarketing/';
+};
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://luxbase.github.io',
-    base: '/qs/'
+    base: getBase()
 });
+
